a09f341395ee4236e76010ec0ef6a77d75605f5e
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2020-05-21 17:40:00 -0500

    Modify for IllegalFormatConversionException

--- src/main/java/com/swirlds/regression/validators/ReconnectValidator.java
+++ src/main/java/com/swirlds/regression/validators/ReconnectValidator.java
@@ -214,8 +214,8 @@ public class ReconnectValidator extends NodeValidator {
 			double roundSup = nodeCsv.getColumn(ROUND_SUPER_MAJORITY).getLastEntryAsDouble();
 
 			if (roundSup < savedStateStartRoundNumber) {
-				addError(String.format("Node %d 's last Entry of roundSup %d is less than " +
-								"savedStateStartRoundNumber %d",
+				addError(String.format("Node %d 's last Entry of roundSup %f is less than " +
+								"savedStateStartRoundNumber %f",
 						reconnectNodeId, roundSup, savedStateStartRoundNumber));
 				isValid = false;
 			}
