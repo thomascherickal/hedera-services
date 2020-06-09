4c744ec9ee2978eee6b859da9daeaedf5e97ed2a
Jeffrey Tang <jeffrey@swirlds.com>
2020-06-09 10:29:19 -0500

    update json

--- src/main/resources/block_sync_port.sh
+++ src/main/resources/block_sync_port.sh
@@ -23,11 +23,11 @@ while true; do
     echo "Block input and output sync port" >> exec.log
     sudo -n iptables -A INPUT -p tcp --dport 40124:40224 -j DROP
     sudo -n iptables -A OUTPUT -p tcp --sport 40124:40224 -j DROP
-    sleep 12
+    sleep $(( ( RANDOM % 5 )  + 8 ))
 
     echo "Enable input and output sync port" >> exec.log
     sudo -n iptables -D INPUT -p tcp --dport 40124:40224 -j DROP
     sudo -n iptables -D OUTPUT -p tcp --sport 40124:40224 -j DROP
-    sleep 12
+    sleep $(( ( RANDOM % 5 )  + 8 ))
 done
 
