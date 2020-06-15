38c599fc459b79364139b74cf38786c3619be788
ljianghedera <leo.jiang@hedera.com>
2020-06-15 10:54:03 -0500

    Adjust master branch slack channel and fix a typo.

--- .circleci/scripts/call-svcs-app-slack.sh
+++ .circleci/scripts/call-svcs-app-slack.sh
@@ -5,5 +5,15 @@ set +e
 
 ensure_slackclient
 
+# If the branch is master, adjust the hedera channel to default one.
+if [ "${CIRCLE_BRANCH}" = "master" ]
+then
+  final_args="$* -c hedera-regression"
+else
+  final_args="$*"
+fi
+
+echo "final_args: $final_args"
+
 SLACK_API_TOKEN=xoxb-344480056389-890228995125-MfvKfFwJtL0ba2Ms8HDf8656 \
-  python3 ${REPO}/.circleci/scripts/svcs-app-slack.py $*
+   python3 ${REPO}/.circleci/scripts/svcs-app-slack.py $final_args
