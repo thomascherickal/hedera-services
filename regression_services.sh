a6e4fa99db57b3badc1fa56df58264ae9a6a3903
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2020-06-18 10:36:50 -0500

    add regression-services.sh

---
+++ regression_services.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+cd "`dirname "$0"`"
+configFile="./configs/$1"
+echo $configFile
+
+eval $(ssh-agent)
+ssh-add /home/ubuntu/.ssh/regression_rsa
+
+cd ../..
+cd services-hedera
+mvn -DskipTests clean install
+
+cd regression
+export aws_access_key_id=AKIAJJST62PF5EO3CMAQ
+export aws_secret_access_key=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
+java \
+-Daws.accessKeyId=AKIAJJST62PF5EO3CMAQ \
+-Daws.secretKey=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 \
+-Dlog4j.configurationFile=log4j2-jrs.xml \
+-Dspring.output.ansi.enabled=ALWAYS \
+-jar regression.jar "$configFile"
