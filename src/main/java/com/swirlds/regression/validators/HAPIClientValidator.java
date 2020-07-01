8bfc2edec8f10a0afe4a4990fdb08e7172e8f701
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2020-07-01 14:57:54 -0500

    fix configs

--- src/main/java/com/swirlds/regression/validators/HAPIClientValidator.java
+++ src/main/java/com/swirlds/regression/validators/HAPIClientValidator.java
@@ -17,12 +17,20 @@
 
 package com.swirlds.regression.validators;
 
+import com.amazonaws.http.conn.ssl.ShouldClearSslSessionPredicate;
 import com.swirlds.regression.logs.LogReader;
 import com.swirlds.regression.logs.services.HAPIClientLogEntry;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class HAPIClientValidator extends Validator {
@@ -35,12 +43,15 @@ public class HAPIClientValidator extends Validator {
 	private int wrongStatus = 0;
 	private int numProblems = 0;
 	private String suiteName = "";
+	private ArrayList<String> failedSpecs = new ArrayList<>();
 
 	private static final String WRONG_STATUS = "wrong status";
 	private static final String STARTING_OF_SUITE = "-------------- STARTING ";
 	private static final String RESULTS_OF_SUITE = "-------------- RESULTS OF ";
 	private static final String EXCEPTION = "Exception";
 	private static final String ERROR = "ERROR";
+
+	private static final String SEPERATOR = "	";
 
 	public HAPIClientValidator(final List<NodeData> testClientNodeData) {
 		this.testClientNodeData = testClientNodeData;
@@ -118,13 +129,14 @@ public class HAPIClientValidator extends Validator {
 		initializeVariables();
 	}
 
-	private boolean checkResultsOfSuite(HAPIClientLogEntry start) {
-		if (!start.getLogEntry().contains(suiteName)) {
+	private boolean checkResultsOfSuite(HAPIClientLogEntry resultEntry) {
+		if (!resultEntry.getLogEntry().contains(suiteName)) {
 			return false;
-		} else if (start.getLogEntry().contains("status=ERROR")) {
-			resultErrorsOfSuite++;
-		} else if (start.getLogEntry().contains("status=PASSED")) {
+		} else if (isPassed(resultEntry)) {
 			resultPassedOfSuite++;
+		} else if (isFailed(resultEntry)) {
+			parseFailedSpecs(resultEntry);
+			resultErrorsOfSuite++;
 		}
 		return true;
 	}
@@ -135,15 +147,31 @@ public class HAPIClientValidator extends Validator {
 	}
 
 	private void validateProblems() {
-		if (wrongStatus > 0 || resultErrorsOfSuite > 0 || numProblems > 0) {
-			addError(String.format("Suite %s has %d wrong status, %d exceptions and " +
-							" %d failed tests",
-					suiteName, wrongStatus, numProblems, resultErrorsOfSuite));
-			isValid &= false;
-		}
+		StringBuilder result = new StringBuilder();
+		result.append(suiteName);
+
 		if (wrongStatus == 0 && resultErrorsOfSuite == 0 && numProblems == 0 && resultPassedOfSuite > 0) {
 			isValid &= true;
-			addInfo(String.format("Suite %s has %d passed results", suiteName, resultPassedOfSuite));
+			result.append(SEPERATOR);
+			result.append("PASSED ");
+			addInfo(result.toString());
+		} else if (wrongStatus > 0 || resultErrorsOfSuite > 0 || numProblems > 0) {
+			if (wrongStatus > 0) {
+				result.append(SEPERATOR);
+				result.append("Wrong Status:" + wrongStatus);
+			}
+			if (numProblems > 0) {
+				result.append(SEPERATOR);
+				result.append("Runtime Exceptions:" + numProblems);
+			}
+			if (resultErrorsOfSuite > 0) {
+				result.append(SEPERATOR);
+				result.append("Failed Tests:" + resultErrorsOfSuite);
+			}
+			result.append(SEPERATOR);
+			result.append(StringUtils.join(failedSpecs, ","));
+			addError(result.toString());
+			isValid &= false;
 		}
 	}
 
@@ -153,5 +181,25 @@ public class HAPIClientValidator extends Validator {
 		wrongStatus = 0;
 		numProblems = 0;
 		suiteName = "";
+		failedSpecs = new ArrayList<>();
 	}
+
+	private boolean isFailed(HAPIClientLogEntry resultEntry) {
+		return resultEntry.getLogEntry().contains("status=FAILED")
+				|| resultEntry.getLogEntry().contains("status=ERROR");
+	}
+
+	private boolean isPassed(HAPIClientLogEntry resultEntry) {
+		return resultEntry.getLogEntry().contains("status=PASSED");
+	}
+
+	private void parseFailedSpecs(HAPIClientLogEntry resultEntry) {
+		failedSpecs.addAll(Stream.of(resultEntry.getLogEntry().
+				split("Spec"))
+				.filter(s -> s.contains("name="))
+				.map(s-> s.substring(s.indexOf("=")+1, s.indexOf("}"))
+						.replace(",", " "))
+				.collect(Collectors.toList()));
+	}
+
 }
