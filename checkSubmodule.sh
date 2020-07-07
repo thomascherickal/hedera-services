63724e35bffc2e9f94e353526892e16791f4c42d
Ron Searcy <ron@swirlds.com>
2020-07-07 18:43:45 -0500

    Added slack submodule pointer class. added calls to the new calss in the checksubmoudle script and made sure the latest version was made with mvn deploy

--- checkSubmodule.sh
+++ checkSubmodule.sh
@@ -7,6 +7,8 @@ cd ..
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 # git pull
 # git submodule update
+echo $JAVA_HOME
+# mvn -DskipTests clean deploy
 
 regress_develop_commit=$(cd regression;git log -n 1 --decorate=short | head -n1 | awk '{print $2}';cd ..)
 regress_sub_commit=$(git submodule status | grep regression | awk '{print $1}')
@@ -16,7 +18,9 @@ apps_sub_commit=$(git submodule status | grep platform-apps | awk '{print $1}')
 
 if [ $regress_develop_commit != $regress_sub_commit ]
 then
-	echo "regression commit doesn't equal develop sub module"
+  regression_develop_information=$(cd regression;git log -n 1)
+  slackMsg="regression commit doesn't equal develop sub module\nplatform regression pointer:$regress_sub_commit\n regression current commit:$regression_develop_information"
+	echo $slackMsg
 else
 	echo "regresion is equal $regress_develop_commit == $regress_sub_commit"
 fi
@@ -27,4 +31,9 @@ then
 else
 	echo "apps is equal"
 fi
+
+cd regression
+#TODO delete this mvn deploy
+mvn -DskipTests clean deploy
+java -cp regression.jar com.swirlds.regression.slack.SlackSubmodulePointerMsg "$slackMsg"
 
--- src/main/java/com/swirlds/regression/slack/SlackSubmodulePointerMsg.java
+++ src/main/java/com/swirlds/regression/slack/SlackSubmodulePointerMsg.java
@@ -1,4 +1,60 @@
+/*
+ * (c) 2016-2020 Swirlds, Inc.
+ *
+ * This software is the confidential and proprietary information of
+ * Swirlds, Inc. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Swirlds.
+ *
+ * SWIRLDS MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SWIRLDS SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+
 package com.swirlds.regression.slack;
 
-public class SlackSubmodulePointerMsg {
+import com.hubspot.slack.client.models.Attachment;
+import com.swirlds.regression.jsonConfigs.SlackConfig;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.swirlds.regression.slack.SlackNotifier.createSlackNotifier;
+
+public class SlackSubmodulePointerMsg extends SlackMsg {
+
+    private static final String SLACK_TOKEN = "xoxp-344480056389-344925970834-610132896599" +
+            "-fb69be9200db37ce0b0d55a852b2a5dc";
+    private static final String SLACK_BOT_TOKEN = "xoxb-344480056389-723753217792-D5RXu4lKOPt3mDFyLTqtSHKo";
+    private static final String SLACK_CHANNEL = "regression-test";
+    private static final String SLACK_FILE_TO_UPLOAD = "./regression/multipage_pdf.pdf";
+    private static final String SLACK_EXPERIMENT_NAME = "SlackUnitTestForFileUpload";
+
+    private static final String SLACK_TEST_FILE_LOCATION = "logs/PTD-FCM1K-success/";
+    private static final String INSIGHT_FILE_LOCATION = "regression/insight.py";
+    private static final String SLACK_REGRESSION_NAME = "Slack Regression Unit Test";
+
+    public SlackSubmodulePointerMsg(SlackConfig slackConfig) {
+        super(slackConfig);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        SlackNotifier sn = createSlackNotifier(
+                SLACK_TOKEN);
+        sn.messageChannel(args[0], "UA5K2LZ1D");
+    }
+    @Override
+    public List<Attachment> generateSlackMessage(StringBuilder stringBuilder) {
+        return null;
+    }
+
+    @Override
+    public String getPlaintext() {
+        return null;
+    }
 }
