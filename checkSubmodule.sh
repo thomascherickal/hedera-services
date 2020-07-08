534addfcb6ed287d65107abc0c721c991f4f14e8
Ron Searcy <ron@swirlds.com>
2020-07-07 21:38:01 -0500

    remove remotes again since git got confused and removed a few commits

--- checkSubmodule.sh
+++ checkSubmodule.sh
@@ -13,20 +13,20 @@ apps_sub_commit=$(git submodule status | grep platform-apps | awk '{print $1}')
 # mvn -DskipTests clean deploy
 cd regression
 git checkout origin/P2161-automatePointerUpdate
-regress_develop_commit=$(git log -n 1 --remotes | head -n1 | awk '{print $2}')
+regress_develop_commit=$(git log -n 1 | head -n1 | awk '{print $2}')
 cd ..
 # regress_sub_commit=$(git log -n 1 --submodule regression | head -n1 | awk '{print $2}')
 
 cd platform-apps
 git checkout origin/develop
-apps_develop_commit=$(git fetch;git log -n 1 --remotes | head -n1 | awk '{print $2}')
+apps_develop_commit=$(git log -n 1 | head -n1 | awk '{print $2}')
 cd ..
 # apps_sub_commit=$(git log -n 1 --submodule platform-apps | head -n1 | awk '{print $2}')
 
 
 if [ $regress_develop_commit != $regress_sub_commit ]
 then
-  regression_develop_information=$(cd regression;git log -n 1 --remotes)
+  regression_develop_information=$(cd regression;git log -n 1)
   slackMsg="regression commit doesn't equal develop sub module
   *platform regression pointer:* $regress_sub_commit
   *regression current commit:*
@@ -36,7 +36,7 @@ fi
 
 if [ $apps_develop_commit != $apps_sub_commit ]
 then
-	apps_develop_information=$(cd platform-apps;git log -n 1 --remotes)
+	apps_develop_information=$(cd platform-apps;git log -n 1)
   slackMsg="$slackMsg
 
   platform-apps commit doesn't equal develop sub module
