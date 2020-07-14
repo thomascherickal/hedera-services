4b42b105dd0936cbfbad23a4325b56c3411cf7fa
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2020-07-14 10:02:58 -0500

    optimize imports

--- src/test/java/com/swirlds/regression/validators/ValidatorTestUtil.java
+++ src/test/java/com/swirlds/regression/validators/ValidatorTestUtil.java
@@ -19,21 +19,14 @@ package com.swirlds.regression.validators;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.swirlds.fcmap.test.lifecycle.ExpectedValue;
-import com.swirlds.fcmap.test.lifecycle.SaveExpectedMapHandler;
-import com.swirlds.fcmap.test.pta.MapKey;
-import com.swirlds.regression.SSHService;
 import com.swirlds.regression.csv.CsvReader;
 import com.swirlds.regression.jsonConfigs.TestConfig;
 import com.swirlds.regression.logs.LogReader;
 import com.swirlds.regression.logs.PlatformLogParser;
-import com.swirlds.regression.logs.services.HAPIClientLogParser;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.SequenceInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -42,8 +35,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.swirlds.regression.RegressionUtilities.*;
-import static com.swirlds.regression.utils.FileUtils.getInputStream;
 import static com.swirlds.regression.validators.RecoverStateValidator.EVENT_MATCH_LOG_NAME;
 import static com.swirlds.regression.validators.StreamType.EVENT;
 import static com.swirlds.regression.validators.StreamType.RECORD;
