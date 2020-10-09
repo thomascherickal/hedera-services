3ad4efb45ad50f161ee3efabe65a4e67b6f1497b
Qian Qiu <qianqiu@swirlds.com>
2020-10-09 10:26:48 -0400

    add a CryptoTransferLoadThenFreezeTest to use it in services restart test and recordStream validate test; #608

---
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/freeze/CryptoTransferLoadThenFreezeTest.java
@@ -0,0 +1,40 @@
+package com.hedera.services.bdd.suites.freeze;
+
+import com.hedera.services.bdd.spec.HapiApiSpec;
+import com.hedera.services.bdd.suites.perf.CryptoTransferLoadTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.freeze;
+
+public class CryptoTransferLoadThenFreezeTest extends CryptoTransferLoadTest {
+	private static final Logger log = LogManager.getLogger(CryptoTransferLoadThenFreezeTest.class);
+
+	public static void main(String... args) {
+		parseArgs(args);
+
+		CryptoTransferLoadTest suite = new CryptoTransferLoadTest();
+		suite.setReportStats(true);
+		suite.runSuiteSync();
+	}
+
+	@Override
+	protected List<HapiApiSpec> getSpecsInSuite() {
+		return List.of(runCryptoTransfers(), freezeAfterTransfers());
+	}
+
+	private HapiApiSpec freezeAfterTransfers() {
+		return defaultHapiSpec("FreezeAfterTransfers").given().when(
+		).then(
+				freeze().startingIn(60).seconds().andLasting(1).minutes()
+		);
+	}
+
+	@Override
+	protected Logger getResultsLogger() {
+		return log;
+	}
+}
